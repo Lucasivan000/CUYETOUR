318da0333384105a8af09590ede91ef440892e58
Lucas Cardozo <lucasivancardozo000@gmail.com>
2026-04-22 22:15:59 -0300

    a

--- data/experiencies.js
+++ data/experiencies.js
@@ -1,21 +1,25 @@
 const EXPERIENCES = [
     {
-        id: "playa-exclusiva",
-        categoria: "playa",
-        titulo: "Día de Playa Exclusivo",
-        precio: "Desde $45.000",
-        descripcion: "Relax total en una playa paradisíaca con servicios premium.",
+        id: "arraial",
+        categoria: "barcos",
+        titulo: "Tour Arraial do Cabo",
+        precio: "Desde R$ 250 por persona",
+        descripcion: "Llamado el “Caribe Brasileño”, Arraial do Cabo es uno de los destinos más buscados de la costa de Río de Janeiro por sus aguas cristalinas y playas de arena blanca. Durante este tour en barco vas a recorrer algunos de los puntos más impresionantes de la región, con paradas para disfrutar del paisaje, nadar y conocer lugares icónicos como la Playa do Forno, la Isla del Faro, la Gruta Azul y el famoso Puente de Atalaia, además del característico perfil de mono en las formaciones rocosas. Es una experiencia completa en contacto directo con el mar, ideal para quienes quieren conocer playas únicas con aguas tranquilas y escenarios naturales inolvidables.",
         imagenes: [
-            "./assets/img/imagen1.jpg",
-            "./assets/img/imagen2.jpg",
-            "./assets/img/imagen3.jpg"
+            "./assets/img/barcos/arraial/arraial1.jpeg",
+            "./assets/img/barcos/arraial/arraial2.jpeg",
+            "./assets/img/barcos/arraial/arraial3.jpeg",
+            "./assets/img/barcos/arraial/arraial4.jpeg",
+            "./assets/img/barcos/arraial/arraial5.jpeg",
+            "./assets/img/barcos/arraial/arraial6.jpeg"
         ],
         detalles: {
             duracion: "6 horas",
             ubicacion: "Costa Atlántica",
-            incluye: "Traslado + guía",
-            noIncluye: "Comidas"
+            incluye: "Transporte ida y vuelta, guía registrado, almuerzo buffet, equipo básico para snorkel",
+            noIncluye: "Comidas y bebidas adicionales"
         }
+
     },
 
     {
